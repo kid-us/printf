e750b255cb0c7080db82c71fb78e0f6e47b96e83
kid-us <kidushh29@gmail.com>
2023-01-24 13:38:59 -0800

    Group Project

--- write_handlers.c
+++ write_handlers.c
@@ -114,14 +114,12 @@ int write_num(int ind, char buffer[],
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
-
 		else if (!(flags & F_MINUS) && padd == ' ')
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
 			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
 		}
-
 		else if (!(flags & F_MINUS) && padd == '0')
 		{
 			if (extra_c)
@@ -130,7 +128,6 @@ int write_num(int ind, char buffer[],
 					write(1, &buffer[ind], length - (1 - padd_start)));
 		}
 	}
-
 	if (extra_c)
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
